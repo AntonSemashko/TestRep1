499cedc097bd4570ffa2200140bb8c1f52819074
Anton Semashko <anton.semashko.jazzteam@gmail.com>
2022-04-14 23:54:54 +0300

    New commit

---
+++ test_script_111.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "*********** Install epel *************"
+echo "*********** Install nfs *************"
+echo "*********** Install Nginx *************"
+echo "*********** Open port *************"
+echo "*********** Install createrepo *************"
+echo "*********** create repo inv *************"
+echo "*********** Downl packedges *************"
+echo "*********** Disable selinux *************"
+echo "*********** Create repository *************"
